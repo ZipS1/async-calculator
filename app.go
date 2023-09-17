977b2c337d73e9ffef8ad56da4be8f1c39e75d25
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2023-09-17 16:34:26 +0300

    In process of making good async structure

--- app.go
+++ app.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
 /*
 калькулятор на горутинах без примитимов синхронизации
 
@@ -8,5 +16,41 @@ package main
 */
 
 func main() {
+	fmt.Print("Input an expression separated with ', ': ")
+	expressions, err := readInput()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	trimNewline(&expressions[len(expressions)-1])
 
+	resultsChannel := make(chan int)
+	for _, exp := range expressions {
+		go calculateExpression(exp, resultsChannel)
+	}
+	time.Sleep(5 * time.Second)
+
+	for i := range resultsChannel {
+		fmt.Println(i)
+	}
+}
+
+func readInput() ([]string, error) {
+	in := bufio.NewReader(os.Stdin)
+	userInput, err := in.ReadString('\n')
+	if err != nil {
+		return []string{}, err
+	}
+
+	var expressions = strings.Split(userInput, ", ")
+	return expressions, nil
+}
+
+func trimNewline(str *string) {
+	*str = strings.TrimSuffix(*str, "\n")
+}
+
+func calculateExpression(exp string, resultChannel chan int) {
+	result := 0
+	resultChannel <- result
 }
